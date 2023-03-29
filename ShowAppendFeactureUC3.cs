f95359952fe467b1a774acc1ad8e9f28e2d7227a
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 20:55:21 +0530

    Check the sequence of entry

---
+++ ShowAppendFeactureUC3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDay14
+{
+    public class ShowAppendFeactureUC3
+    {
+        public Node head;
+
+        public void Add(int Data)
+        {
+            Node node = new Node(Data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+
+            }
+            Console.WriteLine($"{node.Data} inserted into linked list ");
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+
+
+        }
+    }
+}
