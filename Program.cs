8794c9624d0f5941d2701500a0ce4096b87a60c9
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 21:29:35 +0530

    Call Insetion method

--- Program.cs
+++ Program.cs
@@ -8,7 +8,20 @@ namespace LinkedListDay14
         {
             Console.WriteLine("Welcome to Linkedlist problem!");
             Console.WriteLine("**********************************");
-            
+
+            AbilityToInsertUC4 list = new AbilityToInsertUC4();
+            list.Add(56);
+            Console.WriteLine("Node with data 56 is First Created");
+            Console.WriteLine(" ");
+            list.Add(70);
+            Console.WriteLine("Next Append 70 to 56");
+            Console.WriteLine(" ");
+            list.Insertion(2, 30);
+            Console.WriteLine("Finally insert 30 in between 56 and 70:");
+            Console.WriteLine(" ");
+            Console.WriteLine("LinkedList Sequence:");
+            list.Display();
+
         }
     }
 }
