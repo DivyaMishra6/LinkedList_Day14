fc9d0eab82b7ec187df4b72a425f93fd7e7d9026
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 17:56:19 +0530

    Add the value in LinkedList

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListDay14
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linkedlist problem!");
+            Console.WriteLine("**********************************");
+            
+        }
+    }
+}
