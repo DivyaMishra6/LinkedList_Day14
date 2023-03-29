bc2a95c19b57d4576e81447ebc4d458a731304b2
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 20:51:20 +0530

    Show Data Append

--- Program.cs
+++ Program.cs
@@ -9,10 +9,17 @@ namespace LinkedListDay14
             Console.WriteLine("Welcome to Linkedlist problem!");
             Console.WriteLine("**********************************");
 
-            CheckSequenceUC2 list = new CheckSequenceUC2();
+            ShowAppendFeactureUC3 list = new ShowAppendFeactureUC3();
             list.Add(56);
+            Console.WriteLine("Node with data 56 is First Created");
+            Console.WriteLine(" ");
             list.Add(30);
+            Console.WriteLine("Next Append 30 to 56");
+            Console.WriteLine(" ");
             list.Add(70);
+            Console.WriteLine("Finally Append 70 to 30");
+            Console.WriteLine(" ");
+            Console.WriteLine("LinkedList Sequence:");
             list.Display();
         }
     }
